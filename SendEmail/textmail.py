359fe7a5a58dc20633ba5dbdfe719627c30f2b48
jonnypeng <3433201@qq.com>
2018-01-10 10:51:35 +0800

    新增邮件封装

--- SendEmail/textmail.py
+++ SendEmail/textmail.py
@@ -3,8 +3,8 @@
 # __author__ = "Jonny"
 # Email: jonnysps@yeah.net
 # Date: 2017/12/27
-
-import  time
+import os
+import time
 import imaplib
 import smtplib
 from email.mime.text import MIMEText
@@ -12,56 +12,78 @@ from email.header import Header
 
 from email.mime.multipart import MIMEMultipart
 
-# with open("C:\\Users\\Administrator\\Desktop\\SE\\Report\\result2017_12_27_12_02_33.html","rb") as fp:
-# #C:\Users\Administrator\Desktop\SE\Report
-# #C:\Users\Administrator\Desktop\SE\Report\result2017_12_27_12_02_33.html
-#     liset_report=fp.read()
-#     print(liset_report)
-
-# msg_html = MIMEMultipart()
-# msg_html["from"] ="jonnypss@163.com"
-# msg_html["to"] ="jonny.peng@makeblock.com"
-
-nowitme = time.strftime("%Y_%m_%d_%H_%M_%S")
-
-sender = "jonnypss@163.com"#"jonny.peng@makeblock.com " #"jonnypss@163.com"  # 发送邮箱
-
-receiver ="jonny.peng@makeblock.com"  #"jonny.peng@makeblock.com"#"jonnyps@yeah.net"    #dextor.liu@makeblock.com
-
-subject = "Makeblock_1laser-cut自动化报告"+nowitme # 邮件主题
-
-smtpserver = "smtp.163.com"# smtp.exmail.qq.com"  #  smtp.163.com"  # 发送邮件服务器
-
-email_username = "jonnypss@163.com"#"jonny.peng@makeblock.com"#"jonnypss@163.com"  # 发送邮箱账号 密码
-
-email_pwd = "274094800" # "jy123456","274094800"
+#from Test_SePy_base1.SendEmail.textmail2 import git_max_repor
 
 
-msg = MIMEText('你好，文本内容~这里是py输入', 'text','utf-8')
-
-# 设置头
-msg['Subject'] = Header(subject, 'utf-8')
-# 发件人
-msg['from'] = "jonnypss@163.com"
-# 收件人
-msg['to'] = "jonny.peng@makeblock.com"
+report_dir = 'D:/git_local/Test_SePy_base1/Report/'
 
 
+def send_mail(max_file,from_addr="jonny.peng@makeblock.com",to_addr="jonny.peng@makeblock.com",
+              account="jonny.peng@makeblock.com",passwd_ssl="G6sNcvPkE2GWWQPb",
+              smtpserver="smtp.exmail.qq.com"):
+    nowitme = time.strftime("%Y_%m_%d_%H_%M_%S")
 
-
-#smtp=imaplib.IMAP4_SSL("imap.exmail.qq.com",993)
-
-smtp= smtplib.SMTP()
-
-#smtp =smtplib.SMTP_SSL(smtpserver,port=465)
-
-#smtp.starttls()
-smtp.connect(smtpserver)
-
-smtp.set_debuglevel(1)
-
+# sender = "jonny.peng@makeblock.com"  # "jonny.peng@makeblock.com " #"jonnypss@163.com"  # 发送邮箱
 #
-smtp.login(email_username,email_pwd)
-smtp.sendmail(sender,receiver, msg.as_string())  # 把MIMEText 转化成str
-smtp.quit()
+# receiver = "jonny.peng@makeblock.com"  # "jonny.peng@makeblock.com"#"jonnyps@yeah.net"    #dextor.liu@makeblock.com
+#
+# smtpserver = "smtp.exmail.qq.com"  # smtp.exmail.qq.com"  #  smtp.163.com"  # 发送邮件服务器
+#
+# email_username = "jonny.peng@makeblock.com"  # "jonny.peng@makeblock.com"#"jonnypss@163.com"  # 发送邮箱账号 密码
+#
+# email_pwd = "jy123456"  # "jy123456","274094800"
+#
+# email_ssl = "G6sNcvPkE2GWWQPb"
+    #boy = '<p> 哈哈哈哈哈哈哈哈</p>'
+    f= open(max_file, 'rb')
+    mailcontent_s = f.read()
+    f.close()
+    msg = MIMEText(mailcontent_s, _subtype='html', _charset='utf-8')
+#msg = MIMEMultipart()
+    subject = "Makeblock_Laser-cut自动化报告" + nowitme  # 邮件主题
+    # 设置头
+    msg['Subject'] = Header(subject, 'utf-8')
+    # 发件人
+    msg['from'] = "jonny.peng@makeblock.com"
+    # 收件
+    msg['to'] = "jonny.peng@makeblock.com"
 
+#msg.attach(MIMEText(ptah_max_report,_subtype='html',_charset='utf-8'))
+    try:
+        smtp = smtplib.SMTP_SSL(smtpserver,465)
+        smtp.login(account, passwd_ssl)
+
+        smtp.sendmail(from_addr, to_addr, msg.as_string())  # 把MIMEText 转化成str
+        smtp.set_debuglevel(1)
+        smtp.quit()
+        # smtp= smtplib.SMTP()
+        # smtp.connect(smtpserver)
+        # smtp.set_debuglevel(1)
+        # smtp.login(account, passwd_ssl)
+    except Exception as e:
+        raise ('e:', e)
+
+    return
+
+
+def git_max_repor():
+
+    #report_dir = "D:/git_local/Test_SePy_base1/Report/"
+    lists_count = os.listdir(report_dir)
+    # print(type(lists_count))
+    print('排序前的列表： ', lists_count)
+    # print(os.path.getmtime(result_dir))
+    # print(os.path.getctime(result_dir))
+    lists_count.sort(key=lambda x: os.path.getmtime(report_dir +'\\'+ x)if not os.path.isdir(report_dir +'\\'+x) else 0)
+    print("排序后的列表：", lists_count)
+    # 组合目录与最新文件
+    max_file = os.path.join(report_dir, lists_count[-1])
+    print("下标-1的元素：" , lists_count[-1])
+    print(max_file)
+
+    send_mail(max_file)
+
+
+
+if __name__ == "__main__":
+    git_max_repor()
--- SendEmail/textmail2.py
+++ SendEmail/textmail2.py
@@ -13,23 +13,58 @@ from email.header import Header
 
 from email.mime.multipart import MIMEMultipart
 
+#D:\git_local\Test_SePy_base1\Report
+
+# def  git_max_repor():
+#
+#
+#     result_dir = "D:/git_local/Test_SePy_base1/Report/"
+#     lists_count = os.listdir(result_dir)
+#     #print(type(lists_count))
+#     print('排序前的列表：%s' % lists_count)
+#     # print(os.path.getmtime(result_dir))
+#     # print(os.path.getctime(result_dir))
+#     lists_count.sort(key=lambda x: os.path.getmtime(result_dir+'\\'+x)
+#     if not os.path.isdir(result_dir+'\\'+x) else 0)
+#     print("排序后的列表：%s"%lists_count)
+#     maxfile =os.path.join(result_dir,lists_count[-1])
+#     print("下标-1的元素：%s"%lists_count[-1])
+#     print(maxfile)
+
+    # for i in lists_count:
+    #     print('获取的i:',i)
+    #     if i == max(lists_count):
+    #         print(max(lists_count))
+    #         fp=open(os.path.join(result_dir, i))
+    #         max_list_boy = fp.read()
+    #         fp.close()
+    #         # with open(os.path.join(result_dir,i),"rb") as tp:
+    #         #     max_list_boy = tp.read()
+    #         # #     print(os.path.join(result_dir,i))
+    #
+    #         return max_list_boy#print(type(os.path.join(result_dir, i)))
 
 
-result_dir = "C:\\Users\\Administrator\\Desktop\\SE\\Report"
+    #lists_count =os.listdir(result_dir)
 
-lists_fial =os.listdir(result_dir)
+    #lists_pop=lists_count[:-1]
+    #print(lists_count)
+    #print(lists_pop)
 
-lists_pop=lists_fial[:-2]
-print(lists_fial)
-print(lists_pop)
+    #b=os.path.getmtime(lists_pop)
+    #列表最大值
 
-#b=os.path.getmtime(lists_pop)
-#列表最大值
-b=max(lists_pop)
-#a=lists_pop.reverse()
-#a=lists_fial[0]
-#a=lists_fial.sort(key=lambda x: os.path.getmtime(lists_fial+'\\'+lists_fial) )
+    #a=lists_pop.reverse()
+    #a=lists_fial[0]
+    #a=lists_fial.sort(key=lambda x: os.path.getmtime(lists_fial+'\\'+lists_fial) )
 
 
-#print(a)
-print(b)
+    #print(a)
+# g=git_max_repor()
+# print(g)
+
+
+
+git_max_repor()
+
+
